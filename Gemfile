621bef39496bb27d1f9bf06ac9eff338ed949a6d
Simon Tokumine <si@tinypla.net>
2013-01-18 00:43:45 +0000

    bye bye TMS, progress bar

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'http://rubygems.org'
 
 gem 'sqlite3', '1.3.7'
 gem 'chunky_png', '1.2.7'
+gem 'ruby-progressbar'
--- plump.rb
+++ plump.rb
@@ -12,6 +12,7 @@ require 'sqlite3'
 require './lib/global_mercator'
 require 'digest/md5'
 require 'chunky_png'
+require 'ruby-progressbar'
 
 bounds     = nil
 minzoom    = nil
@@ -29,6 +30,14 @@ else
 end
 
 db = SQLite3::Database.new mbtiles
+
+# optimize DB connection
+db.busy_timeout 1000
+db.execute("PRAGMA synchronous=0")
+db.execute("PRAGMA locking_mode=EXCLUSIVE")
+db.execute("PRAGMA journal_mode=DELETE")
+
+# configure 
 db.execute( "select count(*) from map" )  { |row| tile_start = row[0] }
 db.execute( "select * from metadata where name = 'bounds'" )  { |row| bounds  =  row[1].split(",") }
 db.execute( "select * from metadata where name = 'minzoom'" ) { |row| minzoom =  row[1].to_i       }
@@ -44,26 +53,43 @@ ax, ay, bx, by = bounds[0].to_f, bounds[1].to_f, bounds[2].to_f, bounds[3].to_f
 amx, amy = gm.lat_long_to_meters(ay,ax)
 bmx, bmy = gm.lat_long_to_meters(by,bx)
 
-# each zoom
-(minzoom..maxzoom).each do |tz|
+# count total tiles
+tiles = 0
+(minzoom..maxzoom).each do |tz|  
+  apx, apy = gm.meters_to_pixels(amx,amy,tz)
+  bpx, bpy = gm.meters_to_pixels(bmx,bmy,tz)
+  atx, aty = gm.pixels_to_tile(apx,apy)
+  btx, bty = gm.pixels_to_tile(bpx,bpy)
+  agtx, agty = gm.google_tile(atx,aty,tz)
+  bgtx, bgty = gm.google_tile(btx,bty,tz)
+  tiles += ((bgtx-agtx)*(agty-bgty))
+end
+
+
+# start plumping file
+pbar = ProgressBar.create(:title => "PLUMPING...", :total => tiles, :format => '  %t | %c/%C tiles |%P%% |%B')
+(minzoom..maxzoom).each do |tz|  
 
   # meters => pixels => tiles
   apx, apy = gm.meters_to_pixels(amx,amy,tz)
   bpx, bpy = gm.meters_to_pixels(bmx,bmy,tz)
   atx, aty = gm.pixels_to_tile(apx,apy)
   btx, bty = gm.pixels_to_tile(bpx,bpy)
- 
+  agtx, agty = gm.google_tile(atx,aty,tz)  #<= ain't TMS no more
+  bgtx, bgty = gm.google_tile(btx,bty,tz)
+
   # blank tile link => DB if missing
-  (atx..btx).each do |tx|
-    (aty..bty).each do |ty|
+  (agtx..bgtx).each do |tx|
+    (bgty..agty).each do |ty|
       db.execute("INSERT INTO map (zoom_level, tile_column, tile_row, tile_id) 
                  SELECT ?, ?, ?, ?
                  WHERE NOT EXISTS 
                  (SELECT 1 FROM map WHERE zoom_level = ? AND tile_column = ? AND tile_row = ?)", [tz, tx, ty, hash_key, tz, tx, ty])
-      print "."
+      pbar.increment
     end
   end
 end
-db.execute( "select count(*) from map" )  { |row| tile_end = row[0] }
+pbar.finish
 
-puts "\n#{mbtiles} plumped (#{tile_start} => #{tile_end} tiles)"
+db.execute( "select count(*) from map" )  { |row| tile_end = row[0] }
+puts "\n#{mbtiles}: plumped (#{tile_start} => #{tile_end} tiles)"  
